dc0bfcb70821ad7f0932aaa82434f94a0595822d
DFCruzz <diegofcruzz@gmail.com>
2023-04-26 16:31:37 -0300

    feat: added findBooking function to repository

---
+++ src/repositories/booking-repository/index.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/config';
+
+export type BookingIdWithRoomAndHotel = {
+  id: number;
+  Room: {
+    name: string;
+    capacity: number;
+    Hotel: {
+      name: string;
+    };
+  };
+};
+
+async function findBooking(bookingId: number): Promise<BookingIdWithRoomAndHotel | null> {
+  return prisma.booking.findFirst({
+    where: {
+      id: bookingId,
+    },
+    select: {
+      id: true,
+      Room: {
+        select: {
+          name: true,
+          capacity: true,
+          Hotel: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+}
