4d644d6328706b2a41856c6e6ea19a71a08ff502
DFCruzz <diegofcruzz@gmail.com>
2023-04-26 17:11:00 -0300

    feat: added createBooking function to repository

--- src/protocols.ts
+++ src/protocols.ts
@@ -1,4 +1,4 @@
-import { Payment, Ticket } from '@prisma/client';
+import { Booking, Payment, Ticket } from '@prisma/client';
 
 export type ApplicationError = {
   name: string;
--- src/repositories/booking-repository/index.ts
+++ src/repositories/booking-repository/index.ts
@@ -32,3 +32,12 @@ async function findBooking(bookingId: number): Promise<BookingIdWithRoomAndHotel
     },
   });
 }
+
+async function createBooking(userId: number, roomId: number) {
+  return prisma.booking.create({
+    data: {
+      userId: userId,
+      roomId: roomId,
+    },
+  });
+}
